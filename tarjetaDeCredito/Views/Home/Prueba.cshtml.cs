d045dd4f02232f6faa3cf3afb4bd0833e91ed74a
ac-04 <carloshernandez.isaias12.2@gmail.com.com>
2021-02-15 20:12:04 -0600

    :tada: first commit

---
+++ tarjetaDeCredito/Views/Home/Prueba.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tarjetaDeCredito.Views.Home
+{
+    public class Prueba : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
